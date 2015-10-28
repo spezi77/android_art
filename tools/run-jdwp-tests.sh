8c0e1ae8041638abbf29ba09e3934997e33a3770
Nicolas Geoffray <ngeoffray@google.com>
2015-10-28 12:11:27 +0000

    Remove obsolete comment and option.
    Boot image from optimizing handles debuggable now.
    As a side effect reduces the adb command line vogar needs to do.
    
    Change-Id: Ib516163dee19d05de22be5a3228f51c8700411cf
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- tools/run-jdwp-tests.sh
+++ tools/run-jdwp-tests.sh
@@ -31,8 +31,6 @@ fi
 
 art="/data/local/tmp/system/bin/art"
 art_debugee="sh /data/local/tmp/system/bin/art"
-# We use Quick's image on target because optimizing's image is not compiled debuggable.
-image="-Ximage:/data/art-test/core.art"
 args=$@
 debuggee_args="-Xcompiler-option --compiler-backend=Optimizing -Xcompiler-option --debuggable"
 device_dir="--device-dir=/data/local/tmp"
