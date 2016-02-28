multi-os-engine/art

Generate an hprof file to test ahat.
This change sets up the infrastructure to write test cases for ahat
that make use of an hprof file automatically generated from a sample
program.

Change-Id: Id11f656afb69c96a26655cc4caeb745ad844f431

tools/ahat/src/Main.java
@@ -16,10 +16,6 @@
 
 package com.android.ahat;
 
-import com.android.tools.perflib.heap.HprofParser;
-import com.android.tools.perflib.heap.Snapshot;
-import com.android.tools.perflib.heap.io.HprofBuffer;
-import com.android.tools.perflib.heap.io.MemoryMappedFileBuffer;
 import com.sun.net.httpserver.HttpServer;
 import java.io.File;
 import java.io.IOException;
@@ -71,15 +67,8 @@ public class Main {
       return;
     }
 
-    System.out.println("Reading hprof file...");
-    HprofBuffer buffer = new MemoryMappedFileBuffer(hprof);
-    Snapshot snapshot = (new HprofParser(buffer)).parse();
-
-    System.out.println("Computing Dominators...");
-    snapshot.computeDominators();
-
-    System.out.println("Processing snapshot for ahat...");
-    AhatSnapshot ahat = new AhatSnapshot(snapshot);
+    System.out.println("Preparing hprof file...");
+    AhatSnapshot ahat = AhatSnapshot.fromHprof(hprof);
 
     InetAddress loopback = InetAddress.getLoopbackAddress();
     InetSocketAddress addr = new InetSocketAddress(loopback, port);

spezi77/android_art

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

tools/ahat/src/Main.java
@@ -16,10 +16,6 @@
 
 package com.android.ahat;
 
-import com.android.tools.perflib.heap.HprofParser;
-import com.android.tools.perflib.heap.Snapshot;
-import com.android.tools.perflib.heap.io.HprofBuffer;
-import com.android.tools.perflib.heap.io.MemoryMappedFileBuffer;
 import com.sun.net.httpserver.HttpServer;
 import java.io.File;
 import java.io.IOException;
@@ -71,15 +67,8 @@ public class Main {
       return;
     }
 
-    System.out.println("Reading hprof file...");
-    HprofBuffer buffer = new MemoryMappedFileBuffer(hprof);
-    Snapshot snapshot = (new HprofParser(buffer)).parse();
-
-    System.out.println("Computing Dominators...");
-    snapshot.computeDominators();
-
-    System.out.println("Processing snapshot for ahat...");
-    AhatSnapshot ahat = new AhatSnapshot(snapshot);
+    System.out.println("Processing hprof file...");
+    AhatSnapshot ahat = AhatSnapshot.fromHprof(hprof);
 
     InetAddress loopback = InetAddress.getLoopbackAddress();
     InetSocketAddress addr = new InetSocketAddress(loopback, port);